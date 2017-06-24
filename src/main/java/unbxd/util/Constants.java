5621bd4888c589a2a23188e0b1df9f74a28b679b
abhidemon <abhishek.singh@unbxd.com>
2017-06-24 14:19:17 +0530

    feat(utils): Added util classes which will contain Constants and other uses

---
+++ src/main/java/unbxd/util/Constants.java
@@ -0,0 +1,11 @@
+package unbxd.util;
+
+/**
+ * Created by abhi on 31/03/17.
+ */
+public class Constants {
+
+    public static final String serverAddressApiUrl = "http://beta.rc.unbxdapi.com/rc/serverlocation";
+    public static final int concurrency = 20;
+
+}
---
+++ src/main/java/unbxd/util/Tup2.java
@@ -0,0 +1,15 @@
+package unbxd.util;
+
+/**
+ * Created by abhi on 30/03/17.
+ */
+public class Tup2<K,V> {
+
+    public final K _1;
+    public final V _2;
+
+    public Tup2(K _1, V _2) {
+        this._1 = _1;
+        this._2 = _2;
+    }
+}
